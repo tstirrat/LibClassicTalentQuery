17a09de18670b3f759473a09e72f2a9cfad13ab6
Tim Stirrat <tstirrat@google.com>
2020-02-23 09:36:54 -0800

    More boilerplate

--- .github/README.md
+++ .github/README.md
@@ -4,3 +4,16 @@ Query talents from other players in classic (using the Details protocol)
 
 Loosely based on [LibTalentQuery](https://www.wowace.com/projects/libtalentquery-1-0),
 but uses addon messages instead of `NotifyInspect()`
+
+```lua
+local TalentQuery = LibStub:GetLibrary("LibClassicTalentQuery")
+TalentQuery.RegisterCallback(MyAddon, "TalentQuery_Update")
+
+TalentQuery:Query(unitOrGuid)
+
+-- Update will fire for any query result, and also for any PARTY broadcasts made
+-- by the Details addon.
+function MyAddon:TalentQuery_Update(e, name, server, talents)
+  -- ...
+end
+```
--- LibClassicTalentQuery.lua
+++ LibClassicTalentQuery.lua
@@ -1,5 +1,57 @@
 local MAJOR, MINOR = "LibClassicTalentQuery", "0.9.0-@project-version@"
 local LibClassicTalentQuery = LibStub:NewLibrary(MAJOR, MINOR)
 
-if (LibClassicTalentQuery) then
+--[[
+  Usage:
+
+  local TalentQuery = LibStub:GetLibrary("LibClassicTalentQuery")
+  TalentQuery.RegisterCallback(MyAddon, "TalentQuery_Update")
+
+  TalentQuery:Query(unitOrGuid)
+
+  -- Update will fire for any query result, and also for any PARTY broadcasts made
+  -- by the Details addon.
+  function MyAddon:TalentQuery_Update(e, name, server, talents)
+    raidTalents[UnitGUID(unitid)] = spec
+  end
+]]
+local UPDATE_EVENT = "TalentQuery_Update"
+
+local function prdebug(...)
+  print("|cFFFF0000[ClassicTalentQuery]|r", ...)
+end
+
+if not lib then
+  return
+end
+if not lib.events then
+  lib.events = LibStub("CallbackHandler-1.0"):New(lib)
+end
+
+local frame = lib.frame
+if not frame then
+  frame = CreateFrame("Frame", MAJOR .. "_Frame")
+  lib.frame = frame
+end
+frame:UnregisterAllEvents()
+frame:RegisterEvent("CHAT_MSG_ADDON")
+frame:SetScript(
+  "OnEvent",
+  function(this, event, ...)
+    return lib[event](lib, ...)
+  end
+)
+
+function lib:Query(unit)
+  prdebug("Query", unit)
+end
+
+local aceComm = LibStub("AceComm-3.0")
+local LibAceSerializer = LibStub("AceSerializer-3.0")
+local LibDeflate = LibStub("LibDeflate")
+
+function lib:CHAT_MSG_ADDON(prefix, message, channel, sender, target, ...)
+  prdebug("CHAT_MSG_ADDON", prefix, message, channel, sender, target, ...)
+
+
 end
